20572bd992a62df59243fbe9bbd7bcf9704d53b7
Prageeth Monasha <spmonasha@gmail.com>
2024-10-17 21:57:09 +0200

    [dev] fix;

--- Assets/Scripts/ThirdPersonMovement.cs
+++ Assets/Scripts/ThirdPersonMovement.cs
@@ -7,7 +7,7 @@ public class ThirdPersonMovement : MonoBehaviour
     private CharacterController _controller;
     private Transform _camera;
 
-    public float speed = 6f;
+    private float _speed = 6f;
     private const float SmoothTime = 0.1f;
     private float _smoothVelocity;
 
@@ -28,32 +28,29 @@ public class ThirdPersonMovement : MonoBehaviour
     void Update()
     {
         // shift to speed up
-        if (Input.GetKey(KeyCode.LeftShift)) speed = 12;
-        else speed = 6;
+        if (Input.GetKey(KeyCode.LeftShift)) _speed = 12;
+        else _speed = 6;
 
         var horizontal = Input.GetAxis("Horizontal");
         var vertical = Input.GetAxis("Vertical");
 
         var direction = new Vector3(horizontal, 0, vertical).normalized;
 
+        if (direction.y < 0) direction.y = 0f;
+
+        // Changes the height position of the player
+        if (Input.GetButtonDown("Jump")) direction.y += Mathf.Sqrt(JumpHeight * -3.0f * GravityValue);
+        direction.y += GravityValue * Time.deltaTime;
+
         if (direction.magnitude >= 0.1f)
         {
-            if (direction.y < 0) direction.y = 0f;
-
-            // Changes the height position of the player
-            if (Input.GetButtonDown("Jump")) direction.y += Mathf.Sqrt(JumpHeight * -3.0f * GravityValue);
-            direction.y += GravityValue * Time.deltaTime;
-
             var targetRotation = Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg + _camera.eulerAngles.y;
             var rotation = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetRotation, ref _smoothVelocity,
                 SmoothTime);
             transform.rotation = Quaternion.Euler(0, rotation, 0);
 
-
             var moveDirection = Quaternion.Euler(0, targetRotation, 0) * Vector3.forward;
-
-            // _controller.Move(_playerVelocity * Time.deltaTime);
-            _controller.Move(moveDirection.normalized * (speed * Time.deltaTime));
+            _controller.Move(moveDirection.normalized * (_speed * Time.deltaTime));
         }
     }
 
